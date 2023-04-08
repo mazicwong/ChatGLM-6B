d344c13f2517e6f1d70d278f5f7c5ee58bafb7f2
zhiqihuang <zhiqihuang@tencent.com>
2023-04-08 22:05:05 +0800

    .

--- ptuning/train.sh
+++ ptuning/train.sh
@@ -1,15 +1,16 @@
 PRE_SEQ_LEN=128
 LR=2e-2
 
-CUDA_VISIBLE_DEVICES=0 python3 main.py \
+#export CUDA_VISIBLE_DEVICES=1,6
+python main.py \
     --do_train \
-    --train_file AdvertiseGen/train.json \
-    --validation_file AdvertiseGen/dev.json \
+    --train_file /apdcephfs/share_1443437/zhiqihuang/backbone/data/AdvertiseGen/train.json \
+    --validation_file /apdcephfs/share_1443437/zhiqihuang/backbone/data/AdvertiseGen/dev.json \
     --prompt_column content \
     --response_column summary \
     --overwrite_cache \
-    --model_name_or_path THUDM/chatglm-6b \
-    --output_dir output/adgen-chatglm-6b-pt-$PRE_SEQ_LEN-$LR \
+    --model_name_or_path /apdcephfs/share_1443437/zhiqihuang/glm \
+    --output_dir /apdcephfs/share_1443437/zhiqihuang/ChatGLM-6B/output/adgen-chatglm-6b-pt-$PRE_SEQ_LEN-$LR \
     --overwrite_output_dir \
     --max_source_length 64 \
     --max_target_length 64 \
@@ -21,6 +22,7 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py \
     --logging_steps 10 \
     --save_steps 1000 \
     --learning_rate $LR \
-    --pre_seq_len $PRE_SEQ_LEN \
-    --quantization_bit 4
+    --pre_seq_len $PRE_SEQ_LEN
+#     \
+#    --quantization_bit 4
 
